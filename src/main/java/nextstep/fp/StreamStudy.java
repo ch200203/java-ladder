7c821b092b6c1160acedd79ff75dbc8c545d912f
ch200203 <ch200205@naver.com>
2023-12-02 18:05:13 +0900

    refactor : 피드백 반영

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -13,29 +13,31 @@ public class StreamStudy {
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+            .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         long count = 0;
         for (String w : words) {
-            if (w.length() > 12) count++;
+            if (w.length() > 12) {
+                count++;
+            }
         }
         return count;
     }
 
     public static void printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
-                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+            .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
-        words.stream()
+        String joinedWords = words.stream()
             .filter(word -> word.length() > 12)
             .sorted(StreamStudy::compare)
             .distinct()
             .map(word -> word.toLowerCase())
             .limit(100)
-            .forEach(System.out::println);
+            .collect(Collectors.joining("\n"));
+        System.out.println(joinedWords);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
